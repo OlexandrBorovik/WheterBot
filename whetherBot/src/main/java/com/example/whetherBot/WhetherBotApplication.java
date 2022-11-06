77de847095d69d87ea5b278221e867f4efea4223
OlexandrBorovik <100314860+OlexandrBorovik@users.noreply.github.com>
2022-11-06 18:42:55 +0200

    Add files via upload

---
+++ whetherBot/src/main/java/com/example/whetherBot/WhetherBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.whetherBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhetherBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhetherBotApplication.class, args);
+	}
+
+}
---
+++ whetherBot/src/test/java/com/example/whetherBot/WhetherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whetherBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhetherBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
