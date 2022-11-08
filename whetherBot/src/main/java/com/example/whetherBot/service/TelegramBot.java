9568ecdfc0fa872221965f4124cc8ebe8ccdcd52
OlexandrBorovik <100314860+OlexandrBorovik@users.noreply.github.com>
2022-11-08 20:58:02 +0200

    Add files via upload

--- whetherBot/src/main/java/com/example/whetherBot/service/TelegramBot.java
+++ whetherBot/src/main/java/com/example/whetherBot/service/TelegramBot.java
@@ -4,28 +4,45 @@ import com.example.whetherBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Slf4j
 public class TelegramBot extends TelegramLongPollingBot {
 
-    private final BotConfig botConfig;
+    private final BotConfig config;
 
-    public TelegramBot(BotConfig botConfig) {
-        this.botConfig = botConfig;
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get a welcome message"));
+        listofCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listofCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listofCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listofCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return botConfig.getBotName();
+        return config.getBotName();
     }
 
     @Override
     public String getBotToken() {
-        return botConfig.getToken();
+        return config.getToken();
     }
 
     @Override
